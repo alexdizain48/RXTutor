e72b3c80f93c9c228e4a593d70426de9829caa0c
Alex <alart48@yandex.ru>
2020-10-03 17:49:04 +0300

    Add Constructor

--- app/src/main/java/com/example/rxtutor/TestGit.java
+++ app/src/main/java/com/example/rxtutor/TestGit.java
@@ -5,6 +5,11 @@ public class TestGit {
    private int id;
     private String name;
 
+    public TestGit(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public int getId() {
         return id;
     }
